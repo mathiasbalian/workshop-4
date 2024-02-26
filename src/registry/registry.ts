0f63a5c3adae44b074b03e7afeb57b65c812b5b2
mathiasbalian <mathias.balian92@gmail.com>
2024-02-26 16:26:01 +0100

    created routes for registry

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -17,11 +17,25 @@ export async function launchRegistry() {
   const _registry = express();
   _registry.use(express.json());
   _registry.use(bodyParser.json());
+  let getNodeRegistryBody: GetNodeRegistryBody = { nodes: [] };
 
   // TODO implement the status route
   _registry.get("/status", (req, res) => {
     res.send("live");
   });
+
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.status(200).json(getNodeRegistryBody);
+  });
+
+  _registry.post("/registerNode", (req: Request<RegisterNodeBody>, res: Response) => {
+    const { nodeId, pubKey } = req.body;
+    getNodeRegistryBody.nodes.push({ nodeId, pubKey });
+    res.status(200).json({ result: "ok" });
+  });
+
+
+
 
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
