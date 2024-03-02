3dba749f9aa711a7b740ca160f5460ad252e9472
mathiasbalian <mathias.balian92@gmail.com>
2024-03-02 17:21:37 +0100

    added /message route

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -2,20 +2,20 @@ import bodyParser from "body-parser";
 import express from "express";
 import {BASE_ONION_ROUTER_PORT, REGISTRY_PORT} from "../config";
 import {Node} from "../registry/registry";
-import {exportPrvKey, exportPubKey, generateRsaKeyPair} from "../crypto";
-
-let lastReceivedEncryptedMessage: string | null = null;
-let lastReceivedDecryptedMessage: string | null = null;
-let lastMessageDestination: number | null = null;
+import {exportPrvKey, exportPubKey, generateRsaKeyPair, rsaDecrypt, symDecrypt} from "../crypto";
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: number | null = null;
+
   let rsaKeyPair = await generateRsaKeyPair();
   let pubKey = await exportPubKey(rsaKeyPair.publicKey);
-  let privateKey = await exportPrvKey(rsaKeyPair.privateKey);
+  let privateKey = rsaKeyPair.privateKey;
 
   let node: Node = { nodeId: nodeId, pubKey: pubKey };
 
@@ -37,8 +37,29 @@ export async function simpleOnionRouter(nodeId: number) {
   });
 
   onionRouter.get("/getPrivateKey", async (req, res) => {
-    res.status(200).json({result: privateKey});
+    res.status(200).json({result: await exportPrvKey(privateKey)});
   });
+
+  onionRouter.post("/message", async (req, res) => {
+    const {message} = req.body;
+    const decryptedKey = await rsaDecrypt(message.slice(0, 344), privateKey);
+    const decryptedMessage = await symDecrypt(decryptedKey, message.slice(344));
+
+    const nextDestination = parseInt(decryptedMessage.slice(0, 10), 10);
+    const remainingMessage = decryptedMessage.slice(10);
+    lastReceivedEncryptedMessage = message;
+    lastReceivedDecryptedMessage = remainingMessage;
+    lastMessageDestination = nextDestination;
+    await fetch(`http://localhost:${nextDestination}/message`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: remainingMessage }),
+    });
+    res.status(200).send("success");
+  });
+
 
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
